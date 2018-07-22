5af7da86bc12cbf633e34e0c2ad174caf6734263
Jason Pitts <juniorjr083@gmail.com>
2018-07-22 03:27:44 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,40 @@
+class Author
+ attr_accessor :name
+ @@post_count = 0
+
+ def  initialize(name)
+    @name = name
+    @posts = []
+
+ end 
+
+ def posts
+    @posts
+
+ end 
+
+
+ def add_post(value)
+ self.posts << value 
+ value.author = self
+    @@post_count +=1
+ end 
+
+
+ def add_post_by_title(value)
+    post = Post.new(value)
+    self.posts << post 
+
+    post.author = self
+    @@post_count +=1
+
+
+ end 
+
+ def self.post_count
+ @@post_count 
+
+ end 
+
+
+end 
